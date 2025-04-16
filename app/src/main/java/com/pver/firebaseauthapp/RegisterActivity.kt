223ca6da382836ca3d6e106ea3c3c19b4cf74a99
PVER <manycarrotsnow@gmail.com>
2025-04-16 17:11:42 +0530

    Firebase Auth Example

--- app/src/main/java/com/pver/firebaseauthapp/RegisterActivity.kt
+++ app/src/main/java/com/pver/firebaseauthapp/RegisterActivity.kt
@@ -7,6 +7,8 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
+// asdf
+
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
